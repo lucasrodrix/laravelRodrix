bf114d8efa59cf46f7e765240ffbb9acedaaaeb9
Lucas Rodrix <lucasrodrigues1985.lfr@gmail.com>
2024-07-15 12:12:06 -0300

    Projeto Base

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# laravelRodrix
-Projeto de Estudo - Curso Laravel11 (Celke)
+## Requisitos
+
+* PHP 8.2 ou Superior
+* Composer
+
+## Como rodar o projeto baixado
+
+Duplicar o arquivo ".env.example" e renomear para ".env".<br>
+
+Instalar as dependencias do PHP
+```
+composer install
+```
+
+Gerar a chave
+```
+php artisan key:generate
+```
+
+Iniciar o projeto criado com Laravel
+```
+php artisan serve
+```
+Acessar o cinteúdo padrão do Laravel
+```
+http://127.0.0.1:8000
+```
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel Rodrix</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+    </head>
+    <body>
+        <h1>Bem-vindo ao Laravel 11</h1>
+        <!-- <p>Data Atual:{{ \Carbon\Carbon::now()->format('d/m/Y H:i:s')}}</p> -->
+    </body>
+</html>
